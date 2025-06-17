27462aa37bcc1cd0c62d5db574ee896c374905c5
dssc_monitor <gcarte9@wgu.edu>
2025-06-17 14:18:29 -0700

    Added exception handling for RuntimeErrors in tsl2591.py

--- sensors/tsl2591.py
+++ sensors/tsl2591.py
@@ -1,14 +1,28 @@
-from .waveshare_TSL2591.TSL2591 import TSL2591
 import board
 import busio
 import adafruit_tsl2591
 
 class TSL2591Sensor:
-    def __init__(self):
-        #self.sensor = TSL2591()
-        i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_tsl2591.TSL2591(i2c)
+    """
+    Interface for the Adafruit TSL2591 High Dynamic Range Digital Light Sensor.
+    """
+    
+    def __init__(self) -> None:
+        """
+        Initializes the I2C connection and sensor instance.
+        """
+        
+        try:            
+            i2c = busio.I2C(board.SCL, board.SDA)
+            self.sensor = adafruit_tsl2591.TSL2591(i2c)
+        except RuntimeError as err:
+            raise ConnectionError(f"[ERROR] Failed to initialize TSL2591 sensor over I2C: {err}") from err
         
     def read_lux(self) -> float:
-        #return self.sensor.Lux
+        """
+        Reads the current ambient light level in lux.
+        
+        Returns:
+            float: Light level in lux.
+        """
         return self.sensor.lux
